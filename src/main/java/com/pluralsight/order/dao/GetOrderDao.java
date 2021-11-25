ebf6d8577e33f662536eee15076c2b3fab0892a9
Jorge Ezequiel Garcia Lopez <garci1784@cougars.csusm.edu>
2021-11-25 13:52:42 -0800

    got information about an order as well as provided exception handling

--- src/main/java/com/pluralsight/order/dao/GetOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/GetOrderDao.java
@@ -33,11 +33,19 @@ public class GetOrderDao {
     public OrderDto getOrderById(ParamsDto paramsDto) {
         OrderDto orderDto = null;
 
-        try (Connection con = null;
+        try (Connection con = database.getConnection();
              PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderId());
              ResultSet rs = createResultSet(ps)
-        ) {
-
+        )
+        {
+            if(rs.next()) // if query returned a result
+            {
+                orderDto = new OrderDto(); // create a new OrderDto obj and assign it to orderDto
+                orderDto.setOrderId(rs.getLong("order_id"));
+                orderDto.setCustomerId(rs.getLong("order_customer_id"));
+                orderDto.setDate(rs.getTimestamp("order_date"));
+                orderDto.setStatus(rs.getString("order_status"));
+            }
         } catch (SQLException ex) {
             ExceptionHandler.handleException(ex);
         }
@@ -53,8 +61,9 @@ public class GetOrderDao {
      * @throws SQLException In case of an error
      */
     private PreparedStatement createPreparedStatement(Connection con, long orderId) throws SQLException {
-
-        return null;
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setLong(1, orderId);
+        return ps;
     }
 
     /**
@@ -64,6 +73,6 @@ public class GetOrderDao {
      * @throws SQLException In case of an error
      */
     private ResultSet createResultSet(PreparedStatement ps) throws SQLException {
-        return null;
+        return ps.executeQuery();
     }
 }
--- src/main/java/com/pluralsight/order/util/ExceptionHandler.java
+++ src/main/java/com/pluralsight/order/util/ExceptionHandler.java
@@ -12,6 +12,9 @@ public class ExceptionHandler {
      * @param sqlException Exception from which information will be extracted
      */
     public static void handleException(SQLException sqlException) {
-
+        System.out.println("Error Code: " + sqlException.getErrorCode()); // error code
+        System.out.println("SQL State: " + sqlException.getSQLState()); // SQL state
+        System.out.println("Message: " + sqlException.getMessage()); // sqlException message
+        sqlException.printStackTrace(); // call exception's stack trace
     }
 }
